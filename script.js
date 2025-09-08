4595e60ba82315ad9968a2659fd5b4de08a7281e
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-09-09 04:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let upper = document.getElementById('fname');
+
+upper.addEventListener("blur", () => {
+let name = upper.value;
+let result = name.toUpperCase();
+upper.value = result;
+
+
+
+	
+});
+					      
+	
+	
